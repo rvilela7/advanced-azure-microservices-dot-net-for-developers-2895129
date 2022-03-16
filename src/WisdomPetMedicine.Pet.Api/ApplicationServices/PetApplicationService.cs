55aa9bc4cfd5070c14f8c029f8c3e32c67d1c864
Rui Vilela <ruivil@gmail.com>
2022-03-16 17:21:55 +0000

    ChapterWork

--- src/WisdomPetMedicine.Pet.Api/ApplicationServices/PetApplicationService.cs
+++ src/WisdomPetMedicine.Pet.Api/ApplicationServices/PetApplicationService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
 using WisdomPetMedicine.Pet.Api.Commands;
+using WisdomPetMedicine.Pet.Api.IntegrationEvents;
+using WisdomPetMedicine.Pet.Domain.Events;
 using WisdomPetMedicine.Pet.Domain.Repositories;
 using WisdomPetMedicine.Pet.Domain.Services;
 using WisdomPetMedicine.Pet.Domain.ValueObjects;
@@ -17,6 +19,20 @@ namespace WisdomPetMedicine.Pet.Api.ApplicationServices
         {
             this.petRepository = petRepository;
             this.breedService = breedService;
+
+            DomainEvents.PetFlaggedForAdoption.Register(c =>
+            {
+                var integrationEvent = new PetFlaggedForAdoptionIntegrationEvent()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Species = c.Species,
+                    Sex = c.Sex,
+                    DateOfBirth = c.DateOfBirth,
+                    Breed = c.Breed,
+                    Color= c.Color,
+                };
+            });
         }
 
         public async Task HandleCommandAsync(CreatePetCommand command)
